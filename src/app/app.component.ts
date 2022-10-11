ffc1b6bf57538872a2f39e4ed05e21dc9d56c6e2
Piotr Powstanski <piotr.powstanski@god.dev>
2022-10-11 12:25:24 +0200

    Prepare initial state of app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,39 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'strategy-with-lazy-loading';
 
-  deleteUser(user: string) {
-    console.log(`Deleting user ${user}`);
-  }
+    constructor() {
+    }
 
-  deactivateUser(user: string) {
-    console.log(`Deactivate user ${user}`);
-  }
+    title = 'strategy-with-lazy-loading';
+
+    actions = ['Delete', 'Deactivate'];
+    users = ['User One', 'User Two'];
+
+    performAction(userAction: string, user: string) {
+        if (userAction === 'Delete') {
+            this.deleteUser(user);
+            return;
+        }
+
+        if (userAction === 'Deactivate') {
+            this.deactivateUser(user);
+            return;
+        }
+
+        console.log(`Unknown action '${userAction}'.`);
+    }
+
+    private deleteUser(user: string): void {
+        console.log(`Delete user:' ${user}'`);
+    }
+
+    private deactivateUser(user: string): void {
+        console.log(`Deactivate user:' ${user}'`);
+    }
 }
