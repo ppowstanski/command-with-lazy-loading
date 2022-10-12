174aeb3ba29d39d89eeba14097e98f1f3f9dedc7
Piotr Powstanski <piotr.powstanski@god.dev>
2022-10-12 16:41:55 +0200

    Final version

--- src/app/user-actions/delete-user/delete-user.module.ts
+++ src/app/user-actions/delete-user/delete-user.module.ts
@@ -1,17 +1,18 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DeleteUserService} from './delete-user.service';
+import {UserAction} from '@app/users-actions';
 
 @NgModule({
     declarations: [],
-    providers: [
-        DeleteUserService
-    ],
-    imports: [
-        CommonModule
-    ]
+    providers: [DeleteUserService],
+    imports: [CommonModule]
 })
-export class DeleteUserModule {
-    constructor(readonly command: DeleteUserService) {
+export class DeleteUserModule implements UserAction {
+    constructor(private command: DeleteUserService) {
+    }
+
+    execute(user: string, callback: () => void): void {
+        this.command.execute(user, callback);
     }
 }
