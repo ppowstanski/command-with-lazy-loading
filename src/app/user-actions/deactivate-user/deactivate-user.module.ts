03b435b83ca5c84ebafdc149fc994c46c2e8f7f6
Piotr Powstanski <piotr.powstanski@god.dev>
2022-10-12 16:41:21 +0200

    Final version

--- src/app/user-actions/deactivate-user/deactivate-user.module.ts
+++ src/app/user-actions/deactivate-user/deactivate-user.module.ts
@@ -1,17 +1,18 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DeactivateUserService} from './deactivate-user.service';
+import {UserAction} from '@app/users-actions';
 
 @NgModule({
   declarations: [],
-  providers: [
-    DeactivateUserService
-  ],
-  imports: [
-    CommonModule
-  ]
+  providers: [DeactivateUserService],
+  imports: [CommonModule]
 })
-export class DeactivateUserModule {
-  constructor(readonly command: DeactivateUserService) {
+export class DeactivateUserModule implements UserAction {
+  constructor(private command: DeactivateUserService) {
+  }
+
+  execute(user: string, callback: () => void): void {
+    this.command.execute(user, callback);
   }
 }
